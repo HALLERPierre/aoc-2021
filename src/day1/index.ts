1e0f222f209458e5f1c05ccb3dba8e4623c198b3
Pierre Haller <pierre.haller@datalegaldrive.com>
2021-12-01 14:10:01 +0100

    Shorter solution

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -11,8 +11,8 @@ const inputData = getInput({
     schema: z.array(z.number().int()),
 });
 
-const getIncreasesCount = (depths: number[]) => {
-    const [, increasesCount] = depths.reduce<[null | number, number]>(
+export const step1 = (data: number[] = inputData) => {
+    const [, increasesCount] = data.reduce<[null | number, number]>(
         ([previousDepth, increases], depth) => {
             if (previousDepth === null || depth <= previousDepth) {
                 return [depth, increases];
@@ -24,35 +24,11 @@ const getIncreasesCount = (depths: number[]) => {
     return increasesCount;
 };
 
-export const step1 = (data: number[] = inputData) => {
-    return getIncreasesCount(data);
-};
-
 export const step2 = (data: number[] = inputData) => {
-    const windows = data.reduce<Array<[number?, number?, number?]>>(
-        (windows, depth, index) => {
-            const windowsIndexes = [index, index - 1, index - 2];
-            windowsIndexes.forEach((windowIndex) => {
-                if (windowIndex >= 0) {
-                    windows[windowIndex] === undefined
-                        ? (windows[windowIndex] = [depth])
-                        : windows[windowIndex].push(depth);
-                }
-            });
-            return windows;
-        },
-        []
-    );
-
-    const validWindows = windows.filter(
-        (window) => window.length === 3
-    ) as Array<[number, number, number]>;
-
-    const windowsDepths = validWindows.reduce<number[]>((depths, window) => {
-        const sum = window.reduce<number>((sum, depth) => sum + depth, 0);
-        depths.push(sum);
-        return depths;
-    }, []);
-
-    return getIncreasesCount(windowsDepths);
+    return data.reduce<number>((count, depth, index, depths) => {
+        if (index - 3 < 0) {
+            return count;
+        }
+        return depths[index - 3] < depth ? count + 1 : count;
+    }, 0);
 };
